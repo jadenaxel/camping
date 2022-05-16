f8193e1e430e4197697d3ba13873841b24652e24
Yonderi Diaz Rivera <jondydiaz07@gmail.com>
2022-05-16 08:38:13 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+
+const App: FC = (): JSX.Element => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+export default App;
